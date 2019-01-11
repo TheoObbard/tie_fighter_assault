638531569e015c437196ffb8cecb6e727ca35d0a
your name here <theo@dali.dartmouth.edu>
2019-01-11 12:31:15 -0800

    refactor lasers to dry up code and add blur

--- bundle.js
+++ bundle.js
@@ -302,21 +302,37 @@ class Shot {
 
   shot1() {
     this.ctx.beginPath();
-    this.ctx.moveTo(this.start / 2 - 200, window.innerHeight);
-    this.ctx.lineTo(this.mouse[0] + 46, this.mouse[1] + 48);
+    let start = (this.start / 2) - 200
+    let endx = this.mouse[0] + 46
+    let endy = this.mouse[1] + 48
+    this.laserEffect(start, [endx, endy])
+    this.ctx.moveTo(start, window.innerHeight);
+    this.ctx.lineTo(endx, endy);
     this.ctx.lineWidth = 5;
-
     this.ctx.strokeStyle = '#fd946e';
     this.ctx.stroke();
   }
 
   shot2() {
     this.ctx.beginPath();
-    this.ctx.moveTo(this.start / 2 + 200, window.innerHeight);
-    this.ctx.lineTo(this.mouse[0] + 46, this.mouse[1] + 48);
+    let start = (this.start / 2) + 200
+    let endx = this.mouse[0] + 46
+    let endy = this.mouse[1] + 48
+    this.laserEffect(start, [endx, endy])
+    this.ctx.moveTo(start, window.innerHeight);
+    this.ctx.lineTo(endx, endy);
     this.ctx.lineWidth = 5;
-
     this.ctx.strokeStyle = '#fd946e';
+    this.ctx.stroke();
+  }
+
+  laserEffect(start, end) {
+    this.ctx.filter = 'blur(5px)';
+    this.ctx.beginPath();
+    this.ctx.moveTo(start, window.innerHeight);
+    this.ctx.lineTo(end[0], end[1]);
+    this.ctx.lineWidth = 30;
+    this.ctx.strokeStyle = 'red';
     this.ctx.stroke();
   }
 
